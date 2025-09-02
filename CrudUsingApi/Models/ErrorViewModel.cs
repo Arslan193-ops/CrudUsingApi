2fd16ddff2018f29d5061e17f6f804e0b1a6e175
Arslan <arslanmian604@gmail.com>
2025-09-02 19:20:40 +0500

    Add project files.

---
+++ CrudUsingApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudUsingApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
