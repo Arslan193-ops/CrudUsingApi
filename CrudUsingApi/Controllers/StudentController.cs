4f91435aa8a678bc310a7d4863bb3a023d07e256
Arslan <arslanmian604@gmail.com>
2025-09-02 20:05:00 +0500

    created the functionality of inserting with api

--- CrudUsingApi/Controllers/StudentController.cs
+++ CrudUsingApi/Controllers/StudentController.cs
@@ -1,28 +1,48 @@
 ﻿using CrudUsingApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CrudUsingApi.Controllers
 {
     public class StudentController : Controller
     {
         private string url = "https://localhost:7212/api/StudentAPI";
-        private HttpClient client = new HttpClient();           
+        private HttpClient client = new HttpClient();
         public IActionResult Index()
         {
             List<Student> students = new List<Student>();
             HttpResponseMessage response = client.GetAsync(url).Result;
 
-            if(response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
                 string result = response.Content.ReadAsStringAsync().Result;
                 var data = JsonConvert.DeserializeObject<List<Student>>(result);
-                if(data != null)
+                if (data != null)
                 {
                     students = data;
                 }
             }
             return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            string data = JsonConvert.SerializeObject(student);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Record Inserted Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
