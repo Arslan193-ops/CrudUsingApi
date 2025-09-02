aa42806498878016867cdac266c849a21f798265
Arslan <arslanmian604@gmail.com>
2025-09-02 19:41:39 +0500

    added view function (can view students table by retriving it from api)

---
+++ CrudUsingApi/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using CrudUsingApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CrudUsingApi.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7212/api/StudentAPI";
+        private HttpClient client = new HttpClient();           
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Student>>(result);
+                if(data != null)
+                {
+                    students = data;
+                }
+            }
+            return View(students);
+        }
+    }
+}
