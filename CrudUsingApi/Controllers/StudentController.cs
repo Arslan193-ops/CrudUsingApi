11813b3a58dd82d99a6e5c390f231179a209f9bc
Arslan <arslanmian604@gmail.com>
2025-09-02 21:12:30 +0500

    Impemented the edit function

--- CrudUsingApi/Controllers/StudentController.cs
+++ CrudUsingApi/Controllers/StudentController.cs
@@ -39,10 +39,42 @@ namespace CrudUsingApi.Controllers
             HttpResponseMessage response = client.PostAsync(url, content).Result;
             if (response.IsSuccessStatusCode)
             {
-                TempData["SuccessMessage"] = "Record Inserted Successfully";
+                TempData["InsertMessage"] = "Record Inserted Successfully";
                 return RedirectToAction("Index");
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Student std = new Student();
+            HttpResponseMessage response = client.GetAsync(url + "/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Student>(result);
+                if (data != null)
+                {
+                  std = data;
+                }
+            }
+            return View(std);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            string data = JsonConvert.SerializeObject(student);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync(url + "/" +student.id, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["UpdateMessage"] = "Record Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
